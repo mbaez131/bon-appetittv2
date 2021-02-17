7381266ea49c39b7ae2bf821fa593c2926122502
R2H <r2h@MacBook-Pro.attlocal.net>
2021-02-17 17:42:16 -0500

    nav-bar added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+const hamburgerIcon = document.querySelector(".hamburger--container");
+const navbar = document.querySelector(".navbar");
 
+hamburgerIcon.addEventListener("click", () => {
+    navbar.classList.toggle("change")
+})
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
