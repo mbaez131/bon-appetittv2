d130fea4514a4b73c740b1781f765e2cb5471fdd
R2H <r2h@MacBook-Pro.attlocal.net>
2021-02-23 16:55:03 -0500

    updates

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,11 +1,50 @@
 import React from 'react'
+import products from './merch.json';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from 'react-router-dom';
 
-const Shop = () => {
-    return (
-        <main>
-            <h1>Shop Page</h1>
-        </main>
-    )
-};
 
-export default Shop;
+export default class Products extends React.Component {
+render() {
+  return (
+    <div className="container">
+      <h1 className="picks">Shop</h1>
+
+
+
+       <ul className="product-list">
+        {
+        products.map((product, index) => {
+          return <li key={index} className="product">
+              <div className="display"><img src={product.img} className="picdim"/></div>
+            <strong className="pursename">{product.name}</strong><br />
+            <span className="product-price">{product.price}</span><br />
+            <div className="pdesc">{product.description}</div>
+            {/* <a href="{product.link}">Buy Now</a> */}
+          </li>
+        })}
+      </ul>
+
+
+
+ 
+
+
+
+    </div>
+  );
+                }
+
+
+
+
+                
+}
+
+
+
+
