9e9b0b83ee1d5426f439fb2b9ca9b173d239eab6
Joshua Lewis <joshualewis@adminisatorsmbp.home>
2021-02-25 17:16:34 -0500

    saving content

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import ReactPlayer from "react-player"
+import ReactPlayer from "react-player";
 
 const Home = () => {
   return (
@@ -12,19 +12,19 @@ const Home = () => {
         <div class="hero--wrapper">
           <h1 class="hero--text">You Are What You Eat ! Bon Appétit !</h1>
           <div></div>
-          {/* <div class="videos">
+          <div class="videos">
             <ReactPlayer url="https://www.youtube.com/watch?v=cTgs_KO7Stc&t=1s" />
-          </div> */}
+          </div>
           <div class="home-logo">
             <img src="./imgs/bon-logo.png" alt="webiste logo" />
           </div>
-          {/* <ReactPlayer url="https://www.youtube.com/watch?v=3zLC2QDNBuc" /> */}
+          <ReactPlayer url="https://www.youtube.com/watch?v=3zLC2QDNBuc" />
 
           <div class="home-logo">
             <img src="./imgs/bon-logo.png" alt="webiste logo" />
           </div>
 
-          {/* <ReactPlayer url="https://www.youtube.com/watch?v=Ow5xpaFBEj8" /> */}
+          <ReactPlayer url="https://www.youtube.com/watch?v=Ow5xpaFBEj8" />
         </div>
       </div>
     </main>
